ad26663ac5fcc8c766a428ffc71b665b764dbeda
Mauricio Junior <mauricio.junior@gmail.com>
2018-01-05 12:43:10 -0800

    add library project to solution and referenced the projects

---
+++ MicrosfferToDo.Library/Commum/EnderecosWebAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosfferToDo.Library.Commum
+{
+    public static class EnderecosWebAPI
+    {
+        public const string _enderecoBase = "http://localhost";
+    }
+}
--- MicrosfferToDo.WPF/MainWindow.xaml.cs
+++ MicrosfferToDo.WPF/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace MicrosfferToDo.WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
     }
 }
