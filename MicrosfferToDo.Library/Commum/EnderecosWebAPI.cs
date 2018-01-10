1850b2bb6957bc785730ccf416d8641b8422c86e
Mauricio Junior <mauricio.junior@gmail.com>
2018-01-09 21:03:20 -0800

    Colocando o endereço final do web service / web api

--- MicrosfferToDo.Library/Commum/EnderecosWebAPI.cs
+++ MicrosfferToDo.Library/Commum/EnderecosWebAPI.cs
@@ -15,7 +15,7 @@ namespace MicrosfferToDo.Library.Commum
     public static class EnderecosWebAPI
     {
         //constantes utilizadas nos outros projetos WebSite, Web API e WPF
-        public const string _enderecoBase = "http://localhost:50307/"; //mudar o endereço base do Web API
+        public const string _enderecoBase = "https://microsffertodo.azurewebsites.net";//"http://localhost:50307/"; //mudar o endereço base do Web API
         public const string _post = "api/AtividadesToDo";
         public const string _get = "api/AtividadesToDo";
         public const string _delete = "api/AtividadesToDo/"; //{Id}
